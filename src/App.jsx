006402028e7cae1b7c53e0c17e6d040962f0236b
enricoricciardella <enrico.ricciardella@gmail.com>
2025-01-15 20:00:59 +0100

    feat: creazione global context

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/Homepage"
 import ChiSiamo from "./components/Chisiamo"
-import {PostList} from "./components/Postlist"
+import { PostList } from "./components/Postlist"
 import AppLayout from "./components/App.layout"
 import PostDetail from "./components/PostDetail"
+import GlobalContext from "./contexts/GlobalContext";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<HomePage />}/>
-            <Route path="/ChiSiamo" element={<ChiSiamo />}/>
-            <Route path="/PostList" element={<PostList />}/>
-            {/* Creo rotta dinamica */}
-            <Route path="/PostList/:id" element={<PostDetail />}/>
-          </Route>
-        </Routes>
-      </Router>
+      <GlobalContext.Provider value={globalProviderValue}>
+        <Router>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/ChiSiamo" element={<ChiSiamo />} />
+              <Route path="/PostList">
+                <Route index element={<PostList />} />
+                <Route path=":id" element={<PostDetail />} />
+              </Route>
+            </Route>
+          </Routes>
+        </Router>
+      </GlobalContext.Provider>
     </>
   )
 }
